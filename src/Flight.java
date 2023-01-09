9841f8e468faecda57c726dca10eb2e6c45cb553
obaidsi <52484343+obaidsi@users.noreply.github.com>
2023-01-08 19:50:27 -0800

    Created TestSeat and updated Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,2 +1,23 @@
 public class Flight {
+    private String flightNum;
+    private String origin;
+    private String destination;
+    private String date;
+    private String[][] seat;
+    public static final int  SEAT_PER_ROW = 6;
+    public static final int  TOTAL_ROWS = 40;
+
+    public Flight(String flightNum, String origin, String destination, String date){
+        this.flightNum = flightNum;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+    }
+    public String getFlightNum(){ return flightNum;}
+    public String getOrigin(){return origin;}
+    public String getDestination(){return destination;}
+    public String getDate(){return date;}
+    public int getPassengerCount(){return 1;} // to be fixed
+    public String getFlightManifest(){return "to be added";}
+    public Seat[] getSeatReference(String custConfirmation){return new Seat[1];} // To be fixed
 }
